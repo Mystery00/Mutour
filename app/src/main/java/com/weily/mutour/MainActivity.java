72395f387928c1756f67b85154a60b9dccb109ed
Mystery00 <mystery0dyl520@gmail.com>
2017-01-01 18:17:26 +0800

    first;

--- app/src/main/java/com/weily/mutour/MainActivity.java
+++ app/src/main/java/com/weily/mutour/MainActivity.java
@@ -1,33 +1,118 @@
 package com.weily.mutour;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
-
+    private static final String TAG = "MainActivity";
+    private boolean isOpen = false;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        initialize();
+        monitor();
+    }
+
+    private void initialize()
+    {
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbar);
+    }
+
+    private void monitor()
+    {
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (drawer.isDrawerOpen(GravityCompat.START))
+                {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else
+                {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (isOpen)
+                {
+                    fab.setImageResource(R.drawable.ic_add);
+                    isOpen = false;
+                } else
+                {
+                    fab.setImageResource(R.drawable.ic_close);
+                    isOpen = true;
+                }
             }
         });
     }
 
+    @Override
+    public void onBackPressed()
+    {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        } else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
--- app/src/main/java/com/weily/mutour/SplashActivity.java
+++ app/src/main/java/com/weily/mutour/SplashActivity.java
@@ -14,5 +14,6 @@ public class SplashActivity extends AppCompatActivity
 
         Intent intent=new Intent(SplashActivity.this,MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }

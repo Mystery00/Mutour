0e9b68ceb56e610e91ab72936c0e8d83a638f2c3
Mystery00 <mystery0dyl520@gmail.com>
2017-01-20 18:12:52 +0800

    ubuntu backup; add the page of post; something went wrong.

--- app/src/main/java/com/weily/mutour/activity/NewPostActivity.java
+++ app/src/main/java/com/weily/mutour/activity/NewPostActivity.java
@@ -1,10 +1,15 @@
 package com.weily.mutour.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,25 +18,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.mystery0.ipicturechooser.iPictureChooser;
+import com.mystery0.ipicturechooser.iPictureChooserListener;
 import com.weily.mutour.R;
-import com.weily.mutour.adapter.PictureAdapter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NewPostActivity extends AppCompatActivity
 {
     private static final String TAG = "NewPostActivity";
+    private static final int PERMISSION = 322;
     private Toolbar toolbar;
     private TextInputLayout book_name;
     private TextInputLayout book_price;
     private TextInputLayout book_contact;
     private TextInputLayout book_remarks;
+    private iPictureChooser pictureChooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        checkPermission();
         initialize();
         monitor();
     }
@@ -44,15 +52,18 @@ public class NewPostActivity extends AppCompatActivity
         book_price = (TextInputLayout) findViewById(R.id.text_book_price);
         book_contact = (TextInputLayout) findViewById(R.id.text_book_contact);
         book_remarks = (TextInputLayout) findViewById(R.id.text_book_remarks);
-        RecyclerView pictures_layout = (RecyclerView) findViewById(R.id.pictures);
+        pictureChooser = (iPictureChooser) findViewById(R.id.picture_chooser);
 
-        pictures_layout.setLayoutManager(new GridLayoutManager(NewPostActivity.this, 3));
-        List<Integer> list = new ArrayList<>();
-        list.add(R.drawable.float_menu_default_icon);
-        list.add(R.drawable.float_menu_default_icon);
-        list.add(R.drawable.float_menu_default_icon);
-        list.add(R.drawable.float_menu_default_icon);
-        pictures_layout.setAdapter(new PictureAdapter(list));
+        pictureChooser.setDataList(new ArrayList<String>(), new iPictureChooserListener()
+        {
+            @Override
+            public void MainClick()
+            {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, iPictureChooser.REQUEST_IMG_CHOOSE);
+            }
+        });
 
         setSupportActionBar(toolbar);
     }
@@ -146,5 +157,43 @@ public class NewPostActivity extends AppCompatActivity
             {
             }
         });
+    }
+
+    private void checkPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission_group.STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.STORAGE}, PERMISSION);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            case iPictureChooser.REQUEST_IMG_CHOOSE:
+                Uri uri = data.getData();
+                if (uri != null)
+                {
+                    pictureChooser.setUpdatedPicture(uri);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION)
+        {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            {
+                Log.i(TAG, "onRequestPermissionsResult: 权限拒绝");
+                finish();
+            }
+        }
     }
 }
